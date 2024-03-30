c5993f788d7c7f4b47dab719cda2931121eca793
chenqin <490358423@qq.com>
2024-03-30 15:58:25 +0800

    support ChatGPT

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use text_to_ascii_art::convert;
 mod execute;
 mod listener;
 mod utils;
+mod ai;
 use std::env;
 use dotenv::dotenv;
 
@@ -23,8 +24,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // listener::fetcher::fetch_address_all_txs(api_key.clone(),"0x0A30ccEda7f03B971175e520c0Be7E6728860b67", 0, 99999999999999).await?;
 
     // utils::tools::get_contract_solidity_code(api_key, "0xB20bd5D04BE54f870D5C0d3cA85d82b34B836405").await?;
-    let is_invoke = listener::fetcher::is_invoke_mixing_service(api_key.clone(),"0x0A30ccEda7f03B971175e520c0Be7E6728860b67", 0, 99999999999999).await?;
-    println!("是否有混币器相关交易:{}", is_invoke);
+    // let is_invoke = listener::fetcher::is_invoke_mixing_service(api_key.clone(),"0x0A30ccEda7f03B971175e520c0Be7E6728860b67", 0, 99999999999999).await?;
+    // println!("是否有混币器相关交易:{}", is_invoke);
+
+    // 测试ChatGPT功能
+    ai::chatgpt::openai().await;
 
     Ok(())
 }
+
